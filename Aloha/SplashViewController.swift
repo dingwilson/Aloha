335d26ad1a134ec23e08259efbf918848cbcc173
Wilson Ding <shodai100@gmail.com>
2017-02-26 01:29:16 -0600

    Added Splash page

---
+++ Aloha/SplashViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SplashViewController.swift
+//  Aloha
+//
+//  Created by Wilson Ding on 2/26/17.
+//  Copyright © 2017 Wilson Ding. All rights reserved.
+//
+
+import UIKit
+import SwiftVideoBackground
+
+class SplashViewController: UIViewController {
+
+    @IBOutlet weak var backgroundVideo: BackgroundVideo!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        backgroundVideo.createBackgroundVideo(name: "Background", type: "mp4", alpha: 0.5)
+    }
+    
+    
+
+}
